9c269f4c55b488b919fbeaa31b628a5cacadce17
Jesper Håkansson <jesper@jesperh.se>
2017-01-11 14:35:53 +0100

    chore: Move Menu to own file

--- src/edit-list.tsx
+++ src/edit-list.tsx
@@ -3,34 +3,16 @@ import {FieldConfig, FormHelper} from 'react-form-helper'
 import {getValue} from 'react-form-helper/dist/src/helpers'
 import {IconButton} from 'react-toolbox/lib/button'
 import Input from 'react-toolbox/lib/input'
-import {IconMenu, MenuItem} from 'react-toolbox/lib/menu'
-import {withMedia} from 'react-with-media'
 import compose from 'recompose/compose'
 import lifecycle from 'recompose/lifecycle'
 import {removeIn, updateIn} from 'redux-decorated'
 import {Row} from 'styled-material/dist/src/layout'
+import {Menu} from './menu'
 import {Table, TableCell, TableHeader, TableRow} from './table'
-
-export const mediaMobile = '(max-width: 500px)'
 
 export const ToolboxInput = ({required: _, ...props}) => <Input {...props} />
 
 const CellInput = props => <TableCell><ToolboxInput {...props} /></TableCell>
-
-const Menu = withMedia(mediaMobile)(({matches, items, ...props}) =>
-  matches
-    ?
-      <IconMenu icon='more_vert' {...props}>
-        {items.map(item =>
-          <MenuItem key={item.value} {...item} />
-        )}
-      </IconMenu>
-    : <Row horizontal='flex-end'>
-        {items.map(item =>
-          <IconButton key={item.value} icon={item.icon} onClick={item.onClick} />
-        )}
-      </Row>
-)
 
 type Props = {
   /**
